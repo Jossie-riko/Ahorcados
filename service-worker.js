7472ede38785067dc8f2bcab84836e53108e1ce9
Jossie-riko <jossie.ricob@uniagustiniana.edu.co>
2025-09-06 21:20:34 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "ahorcado-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./icon-192.png",
+  "./manifest.json"
+];
+
+// Instalación: guarda en caché los archivos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activación: limpia cachés viejos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch: sirve archivos desde el caché primero
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
